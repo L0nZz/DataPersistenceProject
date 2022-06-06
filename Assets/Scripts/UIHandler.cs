ae0339ac1e752c7dee02809ad2f63e521f712db4
L0nZz <alonzo.mendez100@gmail.com>
2022-06-06 16:22:09 -0400

    Scene Data Persistency

--- Assets/Scripts/UIHandler.cs
+++ Assets/Scripts/UIHandler.cs
@@ -5,9 +5,25 @@ using UnityEditor;
 #endif
 public class UIHandler : MonoBehaviour
 {
+public static UIHandler instance;
+[SerializeField] private GameObject mainMenuUI;
+[SerializeField] private GameObject HighScore;
+    private void Awake()
+    {
+        if(instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
     public void StartGame()
     {
         SceneManager.LoadScene(1);
+        mainMenuUI.SetActive(false);
+        HighScore.transform.localPosition = Vector3.up * 210;
     }
     public void ExitGame()
     {
